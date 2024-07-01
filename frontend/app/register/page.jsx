79719bc834232ea66c7930c36e3f45df1a87eab0
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-07-02 01:08:38 +0530

    updated

--- frontend/app/register/page.jsx
+++ frontend/app/register/page.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
 
 export default function page() {
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('...', username, password);
+
+  };
+
+
   return (
-    <div>page</div>
-  )
+    <main className="h-screen relative">
+      <div className="flex flex-col justify-center items-center h-screen font-mono">
+        <form onSubmit={handleSubmit} className="md:w-[500px] bg-[#ffffff16] w-[350px] p-5 shadow-md shadow-neutral-100 space-y-5">
+          <h1 className="text-3xl text-white font-semibold text-center">Register</h1>
+          <Input
+            type="text"
+            placeholder="User Name"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+            required
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <Button fullWidth type="submit">Login</Button>
+          <p className="text-white text-center">
+            Don't have an account? <button type="button"  >Register</button>
+          </p>
+        </form>
+
+        <video 
+          src={require('../../components/Assets/3141207-uhd_3840_2160_25fps.mp4')}
+          autoPlay
+          loop
+          muted
+          className="absolute top-0 -z-20 w-full md:h-full h-[900px] object-cover"
+        />
+      </div>
+    </main>
+  );
 }
+
